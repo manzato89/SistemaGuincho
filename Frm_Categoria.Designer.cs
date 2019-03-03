cc85e817a9fcc4ff303c012a8dc17f497d513e57
manzato89 <manzato89@gmail.com>
2019-03-03 18:23:05 -0300

    Campos do Form Categoria

--- Frm_Categoria.Designer.cs
+++ Frm_Categoria.Designer.cs
@@ -28,20 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView_Categoria = new System.Windows.Forms.DataGridView();
+            this.btn_CancCategoria = new System.Windows.Forms.Button();
+            this.btn_ExcluirCategoria = new System.Windows.Forms.Button();
+            this.btn_CadCategoria = new System.Windows.Forms.Button();
+            this.btn_NovoCategoria = new System.Windows.Forms.Button();
+            this.txt_DescCategoria = new System.Windows.Forms.TextBox();
+            this.lbl_DescCategoria = new System.Windows.Forms.Label();
+            this.txt_codCategoria = new System.Windows.Forms.TextBox();
+            this.lbl_codCategoria = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Categoria)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView_Categoria
+            // 
+            this.dataGridView_Categoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Categoria.Location = new System.Drawing.Point(49, 194);
+            this.dataGridView_Categoria.Name = "dataGridView_Categoria";
+            this.dataGridView_Categoria.Size = new System.Drawing.Size(673, 245);
+            this.dataGridView_Categoria.TabIndex = 25;
+            // 
+            // btn_CancCategoria
+            // 
+            this.btn_CancCategoria.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_CancCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CancCategoria.Location = new System.Drawing.Point(510, 123);
+            this.btn_CancCategoria.Name = "btn_CancCategoria";
+            this.btn_CancCategoria.Size = new System.Drawing.Size(88, 28);
+            this.btn_CancCategoria.TabIndex = 24;
+            this.btn_CancCategoria.Text = "Cancelar";
+            this.btn_CancCategoria.UseVisualStyleBackColor = false;
+            // 
+            // btn_ExcluirCategoria
+            // 
+            this.btn_ExcluirCategoria.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_ExcluirCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ExcluirCategoria.Location = new System.Drawing.Point(400, 123);
+            this.btn_ExcluirCategoria.Name = "btn_ExcluirCategoria";
+            this.btn_ExcluirCategoria.Size = new System.Drawing.Size(88, 28);
+            this.btn_ExcluirCategoria.TabIndex = 23;
+            this.btn_ExcluirCategoria.Text = "Excluir";
+            this.btn_ExcluirCategoria.UseVisualStyleBackColor = false;
+            // 
+            // btn_CadCategoria
+            // 
+            this.btn_CadCategoria.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_CadCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CadCategoria.Location = new System.Drawing.Point(290, 123);
+            this.btn_CadCategoria.Name = "btn_CadCategoria";
+            this.btn_CadCategoria.Size = new System.Drawing.Size(88, 28);
+            this.btn_CadCategoria.TabIndex = 22;
+            this.btn_CadCategoria.Text = "Cadastrar";
+            this.btn_CadCategoria.UseVisualStyleBackColor = false;
+            // 
+            // btn_NovoCategoria
+            // 
+            this.btn_NovoCategoria.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_NovoCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_NovoCategoria.Location = new System.Drawing.Point(184, 123);
+            this.btn_NovoCategoria.Name = "btn_NovoCategoria";
+            this.btn_NovoCategoria.Size = new System.Drawing.Size(88, 28);
+            this.btn_NovoCategoria.TabIndex = 21;
+            this.btn_NovoCategoria.Text = "Novo";
+            this.btn_NovoCategoria.UseVisualStyleBackColor = false;
+            // 
+            // txt_DescCategoria
+            // 
+            this.txt_DescCategoria.Location = new System.Drawing.Point(278, 46);
+            this.txt_DescCategoria.Name = "txt_DescCategoria";
+            this.txt_DescCategoria.Size = new System.Drawing.Size(444, 20);
+            this.txt_DescCategoria.TabIndex = 16;
+            // 
+            // lbl_DescCategoria
+            // 
+            this.lbl_DescCategoria.AutoSize = true;
+            this.lbl_DescCategoria.Location = new System.Drawing.Point(217, 49);
+            this.lbl_DescCategoria.Name = "lbl_DescCategoria";
+            this.lbl_DescCategoria.Size = new System.Drawing.Size(55, 13);
+            this.lbl_DescCategoria.TabIndex = 15;
+            this.lbl_DescCategoria.Text = "Descrição";
+            // 
+            // txt_codCategoria
+            // 
+            this.txt_codCategoria.Location = new System.Drawing.Point(92, 46);
+            this.txt_codCategoria.Name = "txt_codCategoria";
+            this.txt_codCategoria.Size = new System.Drawing.Size(108, 20);
+            this.txt_codCategoria.TabIndex = 14;
+            // 
+            // lbl_codCategoria
+            // 
+            this.lbl_codCategoria.AutoSize = true;
+            this.lbl_codCategoria.Location = new System.Drawing.Point(46, 49);
+            this.lbl_codCategoria.Name = "lbl_codCategoria";
+            this.lbl_codCategoria.Size = new System.Drawing.Size(40, 13);
+            this.lbl_codCategoria.TabIndex = 13;
+            this.lbl_codCategoria.Text = "Código";
             // 
             // Frm_Categoria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(769, 469);
+            this.Controls.Add(this.dataGridView_Categoria);
+            this.Controls.Add(this.btn_CancCategoria);
+            this.Controls.Add(this.btn_ExcluirCategoria);
+            this.Controls.Add(this.btn_CadCategoria);
+            this.Controls.Add(this.btn_NovoCategoria);
+            this.Controls.Add(this.txt_DescCategoria);
+            this.Controls.Add(this.lbl_DescCategoria);
+            this.Controls.Add(this.txt_codCategoria);
+            this.Controls.Add(this.lbl_codCategoria);
             this.Name = "Frm_Categoria";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Categoria";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Categoria)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_Categoria;
+        private System.Windows.Forms.Button btn_CancCategoria;
+        private System.Windows.Forms.Button btn_ExcluirCategoria;
+        private System.Windows.Forms.Button btn_CadCategoria;
+        private System.Windows.Forms.Button btn_NovoCategoria;
+        private System.Windows.Forms.TextBox txt_DescCategoria;
+        private System.Windows.Forms.Label lbl_DescCategoria;
+        private System.Windows.Forms.TextBox txt_codCategoria;
+        private System.Windows.Forms.Label lbl_codCategoria;
     }
 }
